6435851ec883a0b8487065327086801415e7f91f
anjanijha <anjanipremnitrr@gmail.com>
2017-02-16 15:06:27 +0530

    Initial upload of the project

---
+++ src/LogoutServlet.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LogoutServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request,HttpServletResponse response)throws IOException,ServletException{
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		request.getRequestDispatcher("link.html");
+		HttpSession session=request.getSession();
+		session.invalidate();
+		out.print("you are successfully logged out");
+		out.close();
+	}
+
+}
